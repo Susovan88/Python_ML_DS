59891732cd6aa04d051fe26af8d6886a1e0b0254
Susovan Paul <palsusovan88@gmail.com>
2025-07-27 13:59:25 +0530

    flask todolist

---
+++ Flask FrameWork/flask/api.py
@@ -0,0 +1,54 @@
+from flask import Flask,jsonify,request
+
+app=Flask(__name__)
+
+items=[
+    {"id":1,"name":"1","description":"This is item 1"},
+    {"id":2,"name":"2","description":"This is item 2"}
+]
+
+@app.route("/")
+def home():
+    return " Wellcome to my Todo List App."
+
+@app.route("/items",methods=["GET"])
+def get_items():
+    return jsonify(items)
+
+@app.route("/items/<int:item_id>",methods=["GET"])
+def get_item(item_id):
+    item=next((item for item in items if item["id"]==item_id),None)
+    if item is None:
+        return jsonify({"error":"item not found!"})
+    return jsonify(item)
+
+@app.route("/items",methods=["POST"])
+def create_item():
+    if( not request.json or not "name" in request.json):
+        return jsonify({"error":"some error occured!"})
+    print(request.json)
+    new_item={
+        "id":items[-1]["id"]+1 if items else 1,
+        "name":request.json["name"],
+        "description":request.json["description"]
+    }
+    items.append(new_item)
+    return jsonify(new_item)
+
+@app.route("/items/<int:item_id>",methods=["PUT"])
+def change_item(item_id):
+    item=next((item for item in items if item["id"]==item_id),None)
+    if item is None:
+        return jsonify({"error":"item not found!"})
+    item["name"]=request.json.get("name",item["name"])
+    item["decription"]=request.json.get("description",item["description"])
+    return jsonify(item)
+
+@app.route("/items/<int:item_id>",methods=["DELETE"])
+def delete_item(item_id):
+    global items
+    items=[item for item in items if item["id"]!=item_id]
+    return jsonify({"result":"item deleted successfully!"})
+
+if(__name__=="__main__"):
+    app.run(debug=True)
